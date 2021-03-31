9fb8628f39769105889f27527f3da42c20498164
Jdinis8 <joaodinis01@gmail.com>
2021-03-31 23:08:33 +0100

    A

--- t2/mat/v6f.m
+++ t2/mat/v6f.m
@@ -1,3 +1,4 @@
+clear all
 
 pkg load symbolic
 
@@ -50,25 +51,44 @@ Kb = b(10);
 Kd = b(11);
 
 
+G1 = sym(G1, 'r');
+G2 = sym(G2, 'r');
+G3 = sym(G3, 'r');
+G4 = sym(G4, 'r');
+G5 = sym(G5, 'r');
+G6 = sym(G6, 'r');
+G7 = sym(G7, 'r');
+
+C =  sym(C, 'r');
+Kb = sym(Kb, 'r');
+Kd = sym(Kd, 'r');
+
+omega = sym(omega, 'r');
+
 syms vs(t)
 syms v(t)
+syms Zc
 
-vs(t) = -j*exp(j*sym('omega')*t);
+vs(t) = -j*exp(j*omega*t);
 
 Zc = 1/(j*omega*C);
 
+one = sym(1);
+
+notone = sym(-1);
+
     #V1    V2      V3    V4      V5       V6     V7      V8 
-A = [1      0        0    -1       0        0      0      0;
+A = [one      0        0    notone       0        0      0      0;
     -G1  G1+G2+G3  -G2     0      -G3       0      0      0; 
      0    -G2-Kb    G2     0       Kb       0      0      0;
-     0      0       0      1        0       0      0      0;
+     0      0       0      one        0       0      0      0;
      0    -G3       0     -G4   G3+G4+G5   -G5-1/Zc    -G7    G7+1/Zc;
      0     Kb       0      0     -G5-Kb    G5+1/Zc      0     -1/Zc;
      0     0        0     -G6       0       0    G6+G7  -G7;
-     0     0        0    -Kd*G6     1       0    Kd*G6   -1;]
-
+     0     0        0    -Kd*G6     one       0    Kd*G6   notone;]
 
 v(t) = [vs(t); 0; 0; 0; 0; 0; 0; 0];
 
-v(t) = A\v(t)
+v(t) = A\v(t);
+
 
