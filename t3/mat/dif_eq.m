d0c94c258d76cb5ffc612ca1b85bbeb7170202dd
RuiMPMartins <rui.m.p.martins@tecnico.ulisboa.pt>
2021-05-05 12:58:11 +0100

    mat mais um bocado

--- t3/mat/dif_eq.m
+++ t3/mat/dif_eq.m
@@ -2,12 +2,35 @@
 
 pkg load symbolic
 
-##n = 14;
-##A = 230/n;
-##f=50;
-##t0 = 200e-3
-##t=linspace(t0, t0+10/f, 1000);
-##w=2*pi*f;
+syms  n
+syms  A
+syms  f
+syms  t0
+syms  t
+syms  w
+syms  V_ON
+syms  vT
+syms  eta
+syms  N
+syms  Is
+syms  R
+syms  nC
+
+
+
+n = 18.41351;
+A = 230/n;
+f=50;
+t0 = 200e-3
+t=linspace(t0, t0+10/f, 1000);
+w=2*pi*f;
+V_ON = 0.6;
+vT = 0.025;
+eta = 1;
+N = 31;
+Is  = 1e-14;
+R = 200;
+C = 0.00000133
 
 syms w
 syms A
@@ -15,44 +38,27 @@ syms v0(t)
 
 
 ##voltage after wave rectified (no need to simulate that part, pretty straightforward)
-vO = abs(A*sin(w*t))
+syms vR = abs(A*sin(w*t));
 
-##now we have the top node v4 = v0, and the bottom node is gnd, so it's zero
+#R1->infinity so atan->0
+#tOFF = (1/(2*w)) * atan(1/((2*w)*R1*C))
+syms tOFF = 0
 
-##diode equation
-syms nd #number of diodes = 31
-#eta = 1, so no need to declare it
-syms Is #Is = 1
-syms VT #Vt = ?
-syms Id(v) #current diode
+#vC = A*cos(2*w*tOFF)*exp(-(t-tOFF)/(R1*C));
+syms vC = A*cos(2*w*tOFF)
 
-Id(v) = Is*(exp(v/(nd*VT)) - 1)
+syms vL
 
-#current in capacitor is pretty simple: its a phasor and the nodes
-#are the same as the of the diodes, so actually we have 
+if (vC>N*V_ON)
+	vL = N*V_ON;
+	else
+	vL = vC;
+endif
 
-syms ic(t)
-syms C
+syms vD = vL/N;
 
-ic(t) = C*A*w*cos(w*t)*sin(w*t)/abs(sin(w*t))
+syms rd = eta*vT/(Is*exp(vD/(eta*vT)));
 
-#because dv0/dt * C = ic, then we can approximate the derivative by
-#delta's and reach a good approximation of the next iteration
-#v0+ - v0- = ic*delta(t)/C
-#i.e., v0+ = v0- + ic*delta(t)/C,
-#where the delta is something of our choosing
+syms oscilation = N*rd/(N*rd+R);
 
-syms vnext(vbefore)
-syms h
-
-vnext(vbefore) = vbefore + ic(t)*h/C
-
-#then what is left is just the equation for the diode which can be written as
-
-syms f(v5)
-syms R
-
-f(v5) = vnext(vbefore) - v5 - R*Id(v5)
-
-#in our calculations, everything will have values, except for v5, which is what
-#we want to find. Thus, we use newton-raphsons method to find it.
+syms vO(t) = oscilation*cos(w*t);
