ffb9b96384b1f5a671b25e87678d26dc8219655f
Jdinis8 <joaodinis01@gmail.com>
2021-05-21 17:10:26 +0100

    A

--- t4/mat/nosso.m
+++ t4/mat/nosso.m
@@ -1,36 +1,38 @@
 
-##to calculate the DC component, we admit that the capacitor cin is blocking the current
-##from vin, thus v_base is given by the bias circuit coming from vcc
-##thus:
 
-R1 = 80*10^3
-R2 = 20*10^3
-Vcc= 12
+beta1 = 178.7;
+beta2 = 227.3;
+R1 = 80*10^3;
+R2 = 20*10^3;
+Rc = 1*10^3;
+Re = 100;
+Rout = 100;
+Rl = 8;
+Vcc = 12;
+Vbeon = 0.7;
+VT=25e-3;
+VAFN=69.7;
 
+#    Ib1    Ic1 Ie1 Ib2   Ic2 Ie2 IR2 IR1
+A = [-beta1  1   0  0     0   0    0   0;
+     0       0   0 -beta2 1   0    0   0;
+     1       0   0  0     0   0    1  -1;
+     1       1  -1  0     0   0    0   0;
+     0       0   0  1     1  -1    0   0;
+     0       0   0  0     0   0   R2  R1;
+     0      Rc  Re  0     0  Rc    0   0;
+     0       0   0  0  Rout   0    0   0;]
+     
+v = [0; 0; 0; 0; 0; -Vcc; Vcc-Vbeon; Vcc-Vbeon]
 
-Req = R2*R1/(R1+R2)
-Veq = -R2/(R1+R2)*Vcc
+res = A\v
 
-##lets now do the rest of the circuit
+#logo, v0 = vcc - Ic2*R0
 
-beta1 = 178.7
-beta2 = 227.3
-Vbeon = 0.7
-Rc = 1*10^3
-Re = 100
-Rout = 100
+V0 = Vcc-res(5)*Rout
 
-##first, do thevenin equivalent for the rload and capacitor
-  
-   ##Ib1     Ie1  Ic1  Ib2       Ie2 Ic2
-A = [1+beta1 -1    0    0        0   0;
-     0        0    0    1+beta2  -1  0;
-     Req      1    0    0        0   0;
-     0        Re   Rc   Rc       0   0;
-     1       -1    0    0        0   0;
-     0        0    0    1       -1   1;]
+#coiso e tal
 
-
-v = [0; 0; -Veq-Vbeon; -Vcc-Vbeon; 0; 0]
-
-A\v
+gm1=res(2)/VT
+rpi1=beta1/gm1
+ro1=VAFN/res(2)
